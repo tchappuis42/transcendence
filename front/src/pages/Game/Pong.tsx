e2995704673d2e2920cdc6b4d0b629875f491b79
tchappuis42 <tchappui@student.42lausanne.ch>
2023-05-18 03:09:44 +0200

    test pong

--- front/src/pages/Game/Pong.tsx
+++ front/src/pages/Game/Pong.tsx
@@ -1,5 +1,5 @@
 import Navigation from '../../components/Navigation';
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 interface Paddle {
 	x: number;
@@ -16,35 +16,59 @@ interface Game {
 	winner: number;
 }
 
+interface Ball {
+	x: number,
+	y: number,
+	width: number,
+	height: number,
+	resetting: boolean,
+	dx: number,
+	dy: number,
+}
+
 const Pong = () => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const height = 585;
+	const width = 750;
 	const grid = 15;
 	const paddleHeight = grid * 5; // 80
 	const maxPaddleY = 585 - grid - paddleHeight;
+	const paddleSpeed = 5;
+	const ballSpeed = 5;
 
-	const leftPaddleRef = useRef<Paddle>({
+	const [ball, setBall] = useState<Ball>({
+		x: width / 2,
+		y: height / 2,
+		width: grid,
+		height: grid,
+		resetting: false,
+		dx: ballSpeed,
+		dy: -ballSpeed
+	})
+
+	const [leftPaddle, setleftpaddle] = useState<Paddle>({
 		// start in the middle of the game on the left side
 		x: grid * 2,
-		y: 0,
+		y: height / 2 - paddleHeight / 2,
 		width: grid,
 		height: paddleHeight,
 
 		// paddle velocity
-		dy: 0
+		dy: 0,
 	});
 
-	const rightPaddleRef = useRef<Paddle>({
+	const [rightPaddle, setrightpaddle] = useState<Paddle>({
 		// start in the middle of the game on the left side
-		x: 0,
-		y: 0,
+		x: 750 - (grid * 3),
+		y: 585 / 2 - paddleHeight / 2,
 		width: grid,
 		height: paddleHeight,
 
 		// paddle velocity
-		dy: 0
+		dy: 0,
 	});
 
-	const gameInfoRef = useRef<Game>({
+	const [gameInfo, setGameinfo] = useState<Game>({
 		start: 0,
 		score_left: 0,
 		score_right: 0,
@@ -66,6 +90,10 @@ const Pong = () => {
 		}
 	}
 
+	const drawBall = (context: CanvasRenderingContext2D, ball: Ball) => {
+		context.fillRect(ball.x, ball.y, ball.width, ball.height);
+	}
+
 	const drawGame = (context: CanvasRenderingContext2D, gameInfo: Game) => {
 		// draw score
 		context.fillStyle = 'white';
@@ -81,32 +109,99 @@ const Pong = () => {
 		context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
 	}
 
+	const keyDownHandler = (e: KeyboardEvent) => {
+		if (e.key === "w") {
+			setleftpaddle({ ...leftPaddle, dy: -paddleSpeed })
+		}
+		if (e.key === "s") {
+			setleftpaddle({ ...leftPaddle, dy: paddleSpeed })
+		}
+		if (e.key === "q") {
+			setBall({ ...ball, x: width / 2, y: height / 2 })
+		}
+	}
+
+	const keyUpHandler = (e: KeyboardEvent) => {
+		if (e.key === "w") {
+			setleftpaddle({ ...leftPaddle, dy: 0 })
+		}
+		if (e.key === "s") {
+			setleftpaddle({ ...leftPaddle, dy: 0 })
+		}
+	}
+
+	useEffect(() => {
+		const handleGameUpdate = () => {
+			setleftpaddle((prevPaddle) => ({
+				...prevPaddle,
+				y: prevPaddle.y + prevPaddle.dy
+			}));
+			setBall((prevBall) => ({
+				...prevBall,
+				x: prevBall.x + prevBall.dx,
+				y: prevBall.y + prevBall.dy,
+			}))
+		};
+		console.log("ball y = ", ball.y);
+		if (ball.y < grid) {
+			setBall((prevBall) => ({
+				...prevBall,
+				dy: ballSpeed,
+				y: grid,
+			}));
+		}
+		else if (ball.y + grid > height - grid) {
+			setBall((prevBall) => ({
+				...prevBall,
+				dy: -ballSpeed,
+				y: height - grid * 2,
+			}));
+		}
+		if (leftPaddle.y < grid) {
+			setleftpaddle((prevPaddle) => ({
+				...prevPaddle,
+				y: grid,
+			}));;
+		}
+		const gameLoop = setInterval(handleGameUpdate, 1000 / 60);
+
+		return () => {
+			clearInterval(gameLoop);
+		};
+	}, []);
+
+
+
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		if (!canvas)
 			return;
-		canvas.width = 750;
-		canvas.height = 585;
+		canvas.width = width;
+		canvas.height = height;
 		canvas.id = "pong";
 		const context = canvas.getContext("2d");
 		if (!context)
 			return;
-		const leftPaddle = leftPaddleRef.current;
-		leftPaddle.y = canvas.height / 2 - paddleHeight / 2;
-		const rightPaddle = rightPaddleRef.current;
-		rightPaddle.x = canvas.width - grid * 3;
-		rightPaddle.y = canvas.height / 2 - paddleHeight / 2;
-		const gameInfo = gameInfoRef.current;
 		drawMap(context, canvas);
 		drawPaddle(context, leftPaddle, rightPaddle);
 		drawGame(context, gameInfo);
-	}, [])
+		drawBall(context, ball);
+		console.log("y =", leftPaddle.y);
+		window.addEventListener('keydown', keyDownHandler);
+		window.addEventListener('keyup', keyUpHandler);
+		return () => {
+			window.removeEventListener("keydown", keyDownHandler);
+			window.removeEventListener("keyup", keyUpHandler);
+		};
+	}, [leftPaddle, ball]);
 
 	return (
 		<div>
 			<Navigation />
-			<h1>pong page</h1>
-			<canvas ref={canvasRef} />
+			<h1 className='pong'>pong page</h1>
+			<div className='pong'>
+				<canvas ref={canvasRef} />
+			</div>
 		</div>
 	);
 };

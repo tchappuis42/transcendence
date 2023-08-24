285d74af4abf42c9adcd8fa9da60473e39a7bff5
tchappuis42 <tchappui@student.42lausanne.ch>
2023-08-24 02:24:00 +0200

    ternary operator login

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -49,47 +49,11 @@ const Login = () => {
 		setData({ ...data, twoFA: true })
 	}
 
-	if (page) {
-		return (
-			<div className='signup'>
-				<form onSubmit={loginSubmit} id="form">
-					<h1 className='text'>Login</h1>
-					<label htmlFor="email">
-						<input className='input'
-							type="email"
-							name="email"
-							value={data.email}
-							onChange={handleChange}
-							placeholder='email@exemple.com'
-						/>
-					</label>
-					<label htmlFor="password">
-						<input className='input'
-							type={showPassword ? "text" : "password"}
-							name="password"
-							value={data.password}
-							onChange={handleChange}
-							placeholder='password'
-						/>
-						<input type="checkbox" onClick={togglePassword}></input>
-						<a>show password</a>
-					</label>
-					<button type="submit" className='button'>Login</button>
-					<div className='divtest'>
-						Don't have an account?
-						<span onClick={settingPage} >
-							<p className='p'>signup</p>
-						</span>
-					</div>
-				</form>
-			</div >
-		);
-	}
-	else
-		return (
-			<div className='signup'>
-				<form onSubmit={signupSubmit} id="formsignup">
-					<h1 className='text'>Signup</h1>
+	return (
+		<div className='signup'>
+			<form onSubmit={page ? loginSubmit : signupSubmit} id={page ? "form" : "formsignup"}>
+				<h1 className='text'>{page ? "Login" : "Signup"}</h1>
+				{!page &&
 					<label htmlFor="text">
 						<input className='input'
 							type="text"
@@ -98,39 +62,41 @@ const Login = () => {
 							onChange={handleChange}
 							placeholder='user'
 						/>
-					</label>
-					<label htmlFor="email">
-						<input className='input'
-							type="email"
-							name="email"
-							value={data.email}
-							onChange={handleChange}
-							placeholder='email@exemple.com'
-						/>
-					</label>
-					<label htmlFor="password">
-						<input className='input'
-							type={showPassword ? "text" : "password"}
-							name="password"
-							value={data.password}
-							onChange={handleChange}
-							placeholder='password'
-						/>
-						<input type="checkbox" onClick={togglePassword}></input>
-						show password
-						<input type="checkbox" onClick={toggletfa}></input>
-						2FA
-					</label>
-
-					<button className='button' type="submit">Signup</button>
-					<div className='divtest'>
-						Already have an account?
-						<span onClick={settingPage}>
-							<p className='p'>login</p>
-						</span>
-					</div>
-				</form>
-			</div >
-		);
+					</label>}
+				<label htmlFor="email">
+					<input className='input'
+						type="email"
+						name="email"
+						value={data.email}
+						onChange={handleChange}
+						placeholder='email@exemple.com'
+					/>
+				</label>
+				<label htmlFor="password">
+					<input className='input'
+						type={showPassword ? "text" : "password"}
+						name="password"
+						value={data.password}
+						onChange={handleChange}
+						placeholder='password'
+					/>
+					<input type="checkbox" onClick={togglePassword}></input>
+					<a>show password</a>
+					{!page &&
+						<div>
+							<input type="checkbox" onClick={toggletfa}></input>
+							2Fa
+						</div>}
+				</label>
+				<button type="submit" className='button'>Login</button>
+				<div className='divtest'>
+					Don't have an account?
+					<span onClick={settingPage} >
+						<p className='p'>{page ? "signup" : "login"}</p>
+					</span>
+				</div>
+			</form>
+		</div >
+	);
 };
 export default Login;

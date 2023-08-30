7cf11d92bd486dd6915f8bbb802b4e3d34df7227
tchappuis42 <tchappui@student.42lausanne.ch>
2023-08-30 04:22:37 +0200

    socket

--- back/src/game/game.gateway.ts
+++ back/src/game/game.gateway.ts
@@ -1,13 +1,13 @@
 import { Logger } from "@nestjs/common";
-import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 
 @WebSocketGateway({
 	cors: {
-		origin: ['http://localhost:3000/game'],
+		origin: ['http://localhost:3000']
 	}
 })
-export class GameGateway {
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
 	@WebSocketServer()
 	server: Server;
@@ -18,7 +18,12 @@ export class GameGateway {
 	}
 
 	//deconnexion
-	handleDisConnect(client: Socket) {
+	handleDisconnect(client: Socket) {
 		Logger.log("client disconnected:", client.id)
+	}
+
+	@SubscribeMessage('message')
+	handleEvent(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
+		this.server.emit('message', data)
 	}
 }

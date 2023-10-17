8aaeed98f0cc7a2369bbd571ce4083de8bfd603f
tchappuis42 <tchappui@student.42lausanne.ch>
2023-10-17 04:48:56 +0200

    back add friend + get friends list

---
+++ back/src/friends/friends.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Post, Get, Param, UseGuards, Req } from '@nestjs/common';
+import { FriendsService } from './friends.service';
+import { JwtAuthGuard } from 'src/user/user.guard';
+import { Request } from 'express';
+import { UserDto } from 'src/user/dtos/UserDto';
+import { User } from 'src/user/user.entity';
+
+@Controller('friends')
+export class FriendsController {
+	constructor(private readonly frindsService: FriendsService) { }
+
+	@UseGuards(JwtAuthGuard)
+	@Post("/addFriend/:friendName")
+	async addFriend(@Param('friendName') friendName: string, @Req() req: Request) {
+		const user = req.user as User
+		return await this.frindsService.addFriend(user, friendName)
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get("friends")
+	async getFriends(@Req() req: Request) {
+		const user = req.user as User
+		const freinds = await this.frindsService.getFriends(user)
+		return freinds;
+	}
+}
+
+

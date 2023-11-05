e4270b0629d1db4c74afa8fe2ec7a7b03074d50d
tchappuis42 <tchappui@student.42lausanne.ch>
2023-11-05 21:06:31 +0100

    css pong

--- front/src/pages/Game/gameScore.tsx
+++ front/src/pages/Game/gameScore.tsx
@@ -17,7 +17,6 @@ const GameScore = ({ SetPage }: props) => {
 	useEffect(() => {
 		if (socket) {
 			socket.on("score", (data) => {
-				console.log("kevin = ", data);  // -----> data ok
 				setScore(data);
 			});
 		}
@@ -30,9 +29,11 @@ const GameScore = ({ SetPage }: props) => {
 
 	return <div>
 		{score &&
-			<div className='rules'>
-				<h1> {score} </h1>
-				<span onClick={Page}>retourner a la page de jeu</span>
+			<div className="absolute inset-0 flex items-center justify-center">
+				<div className='bg-white h-40 w-64 text-center py-6 sm:h-60 sm:w-[24rem] md:h-72 md:w-[30rem]'>
+					<h1 className="font-bold text-2xl py-6 sm:text-4xl sm:py-12 md:text-5xl md:py-16"> {score} </h1>
+					<span className="hover:font-semibold cursor-pointer hover:text-red-600 sm:text-xl md:text-2xl" onClick={Page}>retourner a la page de jeu</span>
+				</div>
 			</div>
 		}
 	</div>
--- front/src/pages/Game/socketpong.tsx
+++ front/src/pages/Game/socketpong.tsx
@@ -80,11 +80,9 @@ const SocketPong = () => {
 	const mapChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
 		setColor({ ...color, map: event.target.value });
 	}
-	//flex h-full w-full flex-wrap justify-between align-between
 
-	/*bg-white h-1/2 w-3/5"*/
 	return (
-		<div className="flex w-full h-19/20 p-12">
+		<div className="flex w-full h-19/20 p-4 sm:p-12">
 			{!page &&
 				<div className="flex h-full w-full flex-wrap justify-between align-between">
 					<div className="bg-white h-1/2 w-3/5">
@@ -128,14 +126,27 @@ const SocketPong = () => {
 			}
 			{
 				page &&
-				<div>
-					<div className="players">
-						<div className="player">{player1}</div>
-						<div className="player">{player2}</div>
+				<div className="w-full h-full flex items-center">
+					<div className="relative flex flex-col items-center justify-center h-2/3 w-full sm:h-full pt-12">
+						<div className="bg-white w-full h-1/6 min-[845px]:w-[750px] rounded-t-xl flex">
+							<div className="w-1/2 flex">
+								<img src="https://cdn.intra.42.fr/users/5078380b5384a3c1d0c13abb3b2e5522/tchappui.jpg" alt="" className="rounded-tl-xl h-full w-auto" />
+								<div>
+									<h1 className="text-2xl font-semibold pl-3 pt-2 sm:text-3xl sm:pl-4 sm:pt-4">{player1}</h1>
+									<h2 className="text-xl pl-3 pt-2 sm:pl-4 sm:pt-4">score: 540</h2>
+								</div>
+							</div>
+							<div className="w-1/2 flex flex-row-reverse">
+								<img src="https://cdn.intra.42.fr/users/9f5331cff289327a4c7d42c2a66884de/kdi-noce.jpg" alt="" className="rounded-tr-xl h-full w-auto" />
+								<div>
+									<h1 className="text-2xl font-semibold pr-3 pt-2 sm:text-3xl sm:pr-4 sm:pt-4">{player2}</h1>
+									<h2 className="text-xl pr-3 pt-2 sm:pr-4 sm:pt-4">score: 540</h2>
+								</div>
+							</div>
+						</div>
+						<PongTest color={color} />
+						<GameScore SetPage={SetPage} />
 					</div>
-					<GameScore SetPage={SetPage} />
-					<PongTest color={color} />
-					<button onClick={clean}>clean</button>
 				</div>
 			}
 		</div >

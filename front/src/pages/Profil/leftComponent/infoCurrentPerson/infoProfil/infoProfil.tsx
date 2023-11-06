27a561233c7feb6943db83eefb0575ec5feddddf
dino <kevin.dinocera@protonmail.com>
2023-11-06 14:32:31 +0100

    test srollbar tanguy

--- front/src/pages/Profil/leftComponent/infoCurrentPerson/infoProfil/infoProfil.tsx
+++ front/src/pages/Profil/leftComponent/infoCurrentPerson/infoProfil/infoProfil.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import "../../../styleProfilPage/mainCSS.css"
 import "../infoProfilComponent.css"
 import "./infoProfile.css"
-import PPictures from "../../../../image/kdi-noce.jpg"
 import {ButtonsFriends} from "./tools/buttonsFriends"
 import {MyName} from "./tools/personalInformations"
 export const InfoProfileUser = (): JSX.Element => {
--- front/src/pages/Profil/leftComponent/leftComponent.tsx
+++ front/src/pages/Profil/leftComponent/leftComponent.tsx
@@ -10,6 +10,34 @@ interface Props {
 export const LeftComponent = ({className}: Props) => {
 	return (
 		<div className="left-component-main text-xs red-border">
+			<div className="w-full p-4 h-56 bg-white/50 rounded-3xl items-center flex-col flex my-1 sm:w-2/5">
+				<h1 className="text-3xl sm:text-3xl font-bold h-10">Ranking</h1>
+				<div className="h-[9.5rem] w-full  overflow-y-auto">
+					<table className="table-auto w-full">
+						<thead>
+						<tr>
+							<th>rank</th>
+							<th>name</th>
+							<th>points</th>
+						</tr>
+						</thead>
+						<tbody className="text-center">
+						<tr className="text-blue-800">
+							<td>{myIndex + 1}</td>
+							<td>{myRank?.score}</td>
+							<td>{myRank?.username}</td>
+						</tr>
+						{rank.map((user, id) => (
+							<tr key={id} className="">
+								<td>{id + 1}</td>
+								<td className="">{user.score}</td>
+								<td>{user.username}</td>
+							</tr>
+						))}
+						</tbody>
+					</table>
+				</div>
+			</div>
 			{/*<div className="info-profile-component red-border"*/}
 			{/*	 style={{gridTemplateRows: "5fr 2fr 1fr"}}>*/}
 			{/*	<InfoProfilComponent/>*/}

c5959d77e5297e23197807c50491130ef5ff243e
dino <kevin.dinocera@protonmail.com>
2023-11-11 17:11:08 +0100

    set top bar to the top of the page

--- front/src/pages/Profil/topBar/topBar.tsx
+++ front/src/pages/Profil/topBar/topBar.tsx
@@ -4,7 +4,7 @@ import "./components/slanderousMenu.css"
 import {SlanderousMenu} from "./components/slanderousMenu"
 import {useRef} from "react";
 import Navigation from "../../../ui/organisms/Navigation";
-import {Outlet} from "react-router-dom";
+// import {Outlet} from "react-router-dom";
 
 const navigationHome = [
 	{ label: 'Transcendance', url: '/' },
@@ -20,9 +20,8 @@ const navigationOptionsPong = [
 
 export const TopBar = () => {
 	const myRef = useRef<HTMLDivElement | null>(null);
-	// @ts-ignore
 	return (
-		<div className="h-20 px-4 sticky top-0 bg-gray-50 flex flex-row items-end justify-start">
+		<div className="sticky top-0 bg-gray-50 flex flex-row justify-start">
 			<div ref={myRef} className="top-bar">
 				<div className="px-4 font-bold rounded">
 					<Navigation options={navigationHome} />

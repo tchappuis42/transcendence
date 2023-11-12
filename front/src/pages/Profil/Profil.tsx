c0b97e30fe46feb14c598b0b865fddefda8f2ca0
dino <kevin.dinocera@protonmail.com>
2023-11-12 17:10:12 +0100

    begin with left component

--- front/src/pages/Profil/Profil.tsx
+++ front/src/pages/Profil/Profil.tsx
@@ -2,22 +2,26 @@ import * as React from "react"
 import "../../css/index.css"
 import "./styleProfilPage/mainCSS.css"
 import "./styleProfilPage/toolsCss.css"
-// import {LeftComponent} from "./leftComponent/leftComponent"
+import {LeftComponent} from "./leftComponent/leftComponent"
 // import {Leaderboard} from "./middleComponent/leaderboard"
 // import {ChatSide} from "./rightComponent/chatSide"
+import { Button } from "@material-tailwind/react";
 
+export default function Example() {
+	return <Button>Button</Button>;
+}
 /*friends.controller.ts, friends.service.ts*/
 export const Profil = () => {
 	return (
 		<>
 			<div className="mainBox gap-4">
-				<div className="mainTable flex justify-center items-center font-bold"
-					 style={{fontFamily: "helvetica"}}>Profil-page</div>
-				{/*<div className="mainTable grid-cols-1 md:grid-cols-2 lg:grid-cols-3 blue-border">*/}
-					{/*<LeftComponent/>*/}
+				{/*<div className="mainTable flex justify-center items-center font-bold"*/}
+				{/*	 style={{fontFamily: "helvetica"}}>Profil-page</div>*/}
+					<div className="mainTable grid-cols-1 md:grid-cols-2 lg:grid-cols-3 blue-border">
+						<LeftComponent/>
 					{/*<Leaderboard/>*/}
 					{/*<ChatSide/>*/}
-				{/*</div>*/}
+				</div>
 			</div>
 		</>
 	);

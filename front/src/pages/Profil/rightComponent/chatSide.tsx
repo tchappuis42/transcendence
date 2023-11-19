91e799c8ef9631837198bb8332c104126afd1a99
dino <kevin.dinocera@protonmail.com>
2023-11-19 15:36:51 +0100

    add button badge from material-tailwind

--- front/src/pages/Profil/rightComponent/chatSide.tsx
+++ front/src/pages/Profil/rightComponent/chatSide.tsx
@@ -1,12 +1,9 @@
 import * as React from "react"
-import "../styleProfilPage/mainCSS.css"
 import "./chatSide.css"
+import "../styleProfilPage/mainCSS.css"
 import "./chatFriendsInfo/chatFriendsInfo.css"
-
-import { Badge, Button } from "@material-tailwind/react";
 import {BubbleData} from "../middleComponent/leaderboard";
 import {ChatUserHistoric, ChatUserOnline} from "./chatFriendsInfo/chatFriendsInfo";
-import {BubbleBodyLeaderboard, BubbleHeadLeaderboard} from "../middleComponent/leaderboardInfo/leaderboardComponent";
 
 export const ChatSide = () => {
 
@@ -18,76 +15,26 @@ export const ChatSide = () => {
 				<div className="info-chat-component blue-border row-span-3">
 					<table className="border-separate border-spacing-2 w-full items-start">
 						<tbody>
-						<div className="bubble-component">
-							{
-								bubbleData.map((data, index) => (
-									<ChatUserHistoric
-										key={index}
-										index={index + 1}
-										stats={data.stats}
-										name={data.name}
-										user={data.user}
-										className={data.user === "ieie" ? "sticky top-0 bg-blue-200" : undefined}
-									/>
-								))}
-						</div>
+							<div className="bubble-component-chat">
+								{
+									bubbleData.map((data, index) => (
+										<ChatUserHistoric
+											key={index}
+											index={index + 1}
+											stats={data.stats}
+											name={data.name}
+											user={data.user}
+										/>
+									))}
+							</div>
 						</tbody>
 					</table>
-					{/*<div className="chat-component-main red-border">*/}
-				{/*		<div className="chat-component-elements-table pr-3 blue-border">*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*			<ChatUserHistoric/>*/}
-				{/*		</div>*/}
-				{/*	</div>*/}
 				</div>
 				<div className="online-users-component blue-border">
-					{/*<div className="online-component-main red-border">*/}
-				{/*		<div className="online-component-elements-table pr-3 blue-border">*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*		</div>*/}
-				{/*	</div>*/}
+
 				</div>
 				<div className="online-users-component blue-border">
-					{/*<div className="ongame-component-main red-border">*/}
-				{/*		<div className="ongame-component-elements-table pr-3 blue-border">*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*			<ChatUserOnline/>*/}
-				{/*		</div>*/}
-				{/*	</div>*/}
+
 				</div>
 			</div>
 		</div>

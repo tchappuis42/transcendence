ab14f264dfb9a2483149590dd42420f36adb5212
dino <kevin.dinocera@protonmail.com>
2023-11-19 15:36:51 +0100

    add button badge from material-tailwind

--- front/src/pages/Profil/rightComponent/chatFriendsInfo/chatFriendsInfo.tsx
+++ front/src/pages/Profil/rightComponent/chatFriendsInfo/chatFriendsInfo.tsx
@@ -4,50 +4,40 @@ import "./chatFriendsInfo.css"
 import {useEffect, useState} from "react";
 import { Badge, Button } from "@material-tailwind/react";
 
-// export const ChatUserHistoric = () => {
-// 	return(
-// 		<div className="chat-component-elements blue-border">
-// 			<div className="users-component-elements-info col-span-1 red-border">rank</div>
-// 			<div className="users-component-elements-info col-span-3 red-border">name</div>
-// 		</div>
-// 	);
-// }
 interface Props {
 	index: number;
 	user: string;
 	name: string;
 	stats: number;
-	className: string | undefined;
 }
 
-export const ChatUserHistoric = ({index, stats, name, user, className}: Props) => {
-	const [color, setColor] = useState("");
+export const ChatUserHistoric = ({index, stats, name, user}: Props) => {
+	const [colorBadge, setColorBadge] = useState("");
 
 	useEffect(() => {
 		const rankColor = () => {
 			if (index > 0 && index <= 3)
-				setColor("green");
+				setColorBadge("green");
 			else if (index > 3 && index <= 6)
-				setColor("yellow");
+				setColorBadge("yellow");
 			else if (index > 10 && index <= 10)
-				setColor("orange");
+				setColorBadge("orange");
 			else
-				setColor("red");
-			console.log("the color is : ", color, index);
+				setColorBadge("red");
+			console.log("the color is : ", colorBadge, index);
 		}
 		rankColor();
 	}, [index]);
 
 	return (
-		<tr className={className}>
-			<div className={`${className} bubble-main black-border-fine snap-start hover:bg-${color}-200`}>
+		<tr>
+			<div className={`bubble-main black-border-fine snap-start hover:bg-${colorBadge}-200`}>
 				<td className="col-span-1">
-					{/*<Badge color="green">*/}
-					{/*	<Button>*/}
-					{/*		green*/}
+					<Badge color="green">
+						<Button>
 							<div className="bubble-row justify-start">{user}</div>
-					{/*	</Button>*/}
-					{/*</Badge>*/}
+						</Button>
+					</Badge>
 				</td>
 				<td className="col-span-3 flex justify-center">
 					<div className="bubble-row justify-center">{name}</div>

9e3a32f8c58dda0b3b3ec317793b55d9e4342c5d
dino <kevin.dinocera@protonmail.com>
2023-11-19 16:21:21 +0100

    resolve conflict leftComponent infoProfilComponent file rebase

--- front/src/pages/Profil/leftComponent/leftComponent.tsx
+++ front/src/pages/Profil/leftComponent/leftComponent.tsx
@@ -1,6 +1,6 @@
 import "./leftComponent.css"
 import {GameStats} from "./infoCurrentUserStats/componentInfoGame"
-// import {InfoProfilComponent} from "./infoCurrentUser/InfoProfilComponent"
+import {InfoProfilComponent} from "./infoCurrentUser/InfoProfilComponent"
 
 interface Props {
 	className?: string;
@@ -11,7 +11,7 @@ export const LeftComponent = ({className}: Props) => {
 		<div className="left-component-main text-xs">
 			<div className="info-profile-component gray-border"
 				 style={{gridTemplateRows: "5fr 2fr 1fr"}}>
-				{/*<InfoProfilComponent/>*/}
+				<InfoProfilComponent/>
 			</div>
 			<div className="user-stat-component">
 				<GameStats/>

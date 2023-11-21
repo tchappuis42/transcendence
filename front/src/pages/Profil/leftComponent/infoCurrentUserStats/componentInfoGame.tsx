5c7791a907538b74990d81a3f44796a977a98e6f
dino <kevin.dinocera@protonmail.com>
2023-11-21 17:30:12 +0100

    resolve conflict profil mainCSS

--- front/src/pages/Profil/leftComponent/infoCurrentUserStats/componentInfoGame.tsx
+++ front/src/pages/Profil/leftComponent/infoCurrentUserStats/componentInfoGame.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./componentInfoGame.css"
+import {UserGameWin} from "./infoProfil/userGameWin";
 interface Props {
 	stats: number;
 	name: string;
@@ -37,5 +38,63 @@ export const GameStats = () => {
 				</tbody>
 			</table>
 		</div>
+		// <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+		// 	<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+		// 		<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+		// 		<tr>
+		// 			<th scope="col" className="px-6 py-3">
+		// 				Product name
+		// 			</th>
+		// 			<th scope="col" className="px-6 py-3">
+		// 				Color
+		// 			</th>
+		// 		</tr>
+		// 		</thead>
+		// 		<tbody>
+		// 		<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+		// 			<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+		// 				Apple MacBook Pro 17"
+		// 			</th>
+		// 			<td className="px-6 py-4">
+		// 				Silver
+		// 			</td>
+		// 		</tr>
+		// 		<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+		// 			<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+		// 				Microsoft Surface Pro
+		// 			</th>
+		// 			<td className="px-6 py-4">
+		// 				White
+		// 			</td>
+		// 		</tr>
+		// 		<tr className="bg-white dark:bg-gray-800">
+		// 			<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+		// 				Magic Mouse 2
+		// 			</th>
+		// 			<td className="px-6 py-4">
+		// 				Black
+		// 			</td>
+		// 		</tr>
+		// 		</tbody>
+		// 	</table>
+		// </div>
+		// <div className="main-bubble-component green-border" style={{gap: "5px"}}>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// 	<Bubble/>
+		// </div>
 	);
 }

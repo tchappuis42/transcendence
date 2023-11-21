6186740986cc0c0d96e94840a8290c8661d0fbb1
dino <kevin.dinocera@protonmail.com>
2023-11-21 16:13:41 +0100

    resolve topbar conflict

--- front/src/pages/topBar/topBar.tsx
+++ front/src/pages/topBar/topBar.tsx
@@ -21,7 +21,7 @@ const navigationOptionsPong = [
 export const TopBar = () => {
 	const myRef = useRef<HTMLDivElement | null>(null);
 	return (
-		<div className="sticky top-0 bg-gray-50 flex flex-row justify-start">
+		<div className="px-4 sticky top-0 bg-gray-50 flex flex-row items-end justify-start">
 			<div ref={myRef} className="top-bar">
 				<div className="px-4 font-bold rounded">
 					<Navigation options={navigationHome} />

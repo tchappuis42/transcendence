3719b59604ec3aba9a633f24e9e428ec000a301b
dino <kevin.dinocera@protonmail.com>
2023-11-21 16:53:54 +0100

    resolve friends controller services status back and socketpong front

--- back/src/friends/friends.service.ts
+++ back/src/friends/friends.service.ts
@@ -15,6 +15,7 @@ export class FriendsService {
 	async addFriend(user: User, friend: number) {
 		const friendUser = await this.userservice.validateUser(friend);
 		const check = await this.friendsRepository.findOne({ where: [{ first_User: user, second_User: friendUser }, { first_User: friendUser, second_User: user }] })
+		//console.log("check = ", check)
 		if (check)
 			return "demande d'ami déjà envoyé"
 		const friends = new Friends()
@@ -28,6 +29,7 @@ export class FriendsService {
 		const list = await this.friendsRepository.find({ where: [{ first_User: user }, { second_User: user }] })
 		if (!list)
 			throw new NotFoundException("user not found") // bonne erreur
+		console.log("liste = ", list)
 		const friends = list.map((friend) => {
 			if (friend.first_User.id === user.id) {
 				if (friend.friend_status === true)

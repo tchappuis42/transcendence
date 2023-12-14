250b5e3a330a102630a5612a1fcf7816435d3fff
dino <kevin.dinocera@protonmail.com>
2023-12-14 17:17:30 +0100

    rm unesecary comments and add information for dev prod command

--- docker/back/js.sh
+++ docker/back/js.sh
@@ -1,23 +1,14 @@
 # demarrer nest-js en dev
 #npm install -g @nestjs/cli
-# ------ ca sert a rien
-#npm i --save class-validator class-transformer
-#npm install --save @nestjs/typeorm typeorm pg
-#npm i bcrypt
-#npm install --save @nestjs/swagger
-#npm i express-session
-#npm install --save @nestjs/jwt
-#npm install --save @nestjs/passport passport passport-local
-#npm install --save-dev @types/passport-local
-#npm i --save @nestjs/websockets @nestjs/platform-socket.io
-#npm install otplib --save
-#npm i --save-dev @types/qrcode
 #npm run start:dev
-# ------
+
+# demarrer nest-js en prod
 npm i
 npm i -g npm@latest
+
 # demarrer nest-js en dev
 #nest start --watch
+
 # demarrer nest-js en prod
 npm run build
 node dist/main.js
--- docker/front/js.sh
+++ docker/front/js.sh
@@ -1,15 +1,8 @@
-#npm i react
-#npm i axios
-#npm i react-router-dom
-#npm i zustand
-#npm i --save-dev @types/react-modal
-#npm i moment
-#npm i -D tailwindcss
-#npm i @material-tailwind/react
+npm install
+# cmd pour dev front
 #npm start
 
-npm install
-
+# cmd pour prod front
 npm i -g serve
 
 npm run build

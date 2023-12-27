706da43fb2f75e4cf423c93b686d8c0213ffa186
Theophile Bruelhart <93927522+Theophilebrulhart@users.noreply.github.com>
2023-12-27 13:47:58 +0100

    mes trucs à régler

--- front/src/pages/Chat/component/UserInChannelCard.tsx
+++ front/src/pages/Chat/component/UserInChannelCard.tsx
@@ -4,6 +4,7 @@ import { Account } from "../../../ui/types";
 import { handleMouseEnter, handleMouseLeave } from "../../HomePage/Tools";
 import AvatarContainer from "../../HomePage/CardContent/avatarContainer";
 import "./card.css"
+import { useNavigate } from "react-router-dom";
 
 interface userInChannel {
 	userInChannel: Account;
@@ -12,7 +13,7 @@ interface userInChannel {
 const UserInChannelCard = ({ userInChannel }: userInChannel) => {
 
 	const socket = useSocket();
-
+	const navigate = useNavigate();
 	const invitGame = (e: SyntheticEvent) => {
 		e.preventDefault();
 
@@ -21,10 +22,18 @@ const UserInChannelCard = ({ userInChannel }: userInChannel) => {
 		}
 	}
 
+	const handleNav = (id: number) => {
+		navigate("/profil", {
+			state: {
+				id: id
+			}
+		})
+	}
+
 	return (
 		// <div className="h-full bg-white/50 m-2.5 rounded-md shadow-lg box-border flex justify-around items-center cursor-pointer" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-		<div className="main-card border b-slay-200 h-1/6" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-			<div id={"chat"} className="avatar-card">
+		<div className="main-card border b-slay-200 h-1/6">
+			<div id={"chat"} className="avatar-card" onClick={() => handleNav(userInChannel.id)}>
 				<AvatarContainer src={userInChannel.avatar} square={10} navigation={false} id_div={"chat"}/>
 			</div>
 			<div className="name-card col-span-2">

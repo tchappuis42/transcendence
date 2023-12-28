878f4c010b9fd03b74225ba4de23f0c1bde6be2f
Theophile Bruelhart <93927522+Theophilebrulhart@users.noreply.github.com>
2023-12-28 16:42:47 +0100

    shadow conflit

--- front/src/pages/Chat/component/MessageChatCard.tsx
+++ front/src/pages/Chat/component/MessageChatCard.tsx
@@ -15,7 +15,7 @@ const MessageChatCard = ({ msg, index }: messageProps) => {
 	return (
 		msg.uId === account.id ? (
 			<div className="w-full flex justify-end mt-2 px-4">
-				<div className="bg-orange-200 flex p-3 h-full justify-end max-w-2/3 rounded-md shadow-md shadow"
+				<div className="bg-orange-200 flex p-3 h-full justify-end max-w-2/3 rounded-md shadow-md"
 					key={index}
 					style={{ maxWidth: "80%", wordBreak: "break-word" }}
 					onMouseEnter={handleMouseEnter}
@@ -31,7 +31,7 @@ const MessageChatCard = ({ msg, index }: messageProps) => {
 			</div>
 		) : (
 			<div className="w-full flex justify-start mt-2 px-4">
-				<div className="flex p-3 h-full mw-2/3 rounded-md bg-black/70 shadow-md shadow-md flex-col gap-1"
+				<div className="flex p-3 h-full mw-2/3 rounded-md bg-black/70 shadow-md  flex-col gap-1"
 					key={index}
 					style={{ maxWidth: "80%", wordBreak: "break-word" }}
 					onMouseEnter={handleMouseEnter}
